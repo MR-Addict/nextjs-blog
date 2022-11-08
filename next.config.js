35a37b02f77bfa288764b941fb438296f08cd8fe
MR-Addict <2750417853@qq.com>
2022-11-08 10:37:33 +0800

    fix export

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "/";
+let basePath = "";
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY;
   assetPrefix = `/${repo}/`;
-  console.log(repo);
+  basePath = `/${repo}`;
 }
 
 module.exports = {
   assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
